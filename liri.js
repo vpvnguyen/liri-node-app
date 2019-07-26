38358583452e4de2421914e10a06da6c010fdd6d
Vincent Nguyen <vpvnguyen@gmail.com>
2019-07-25 23:21:49 -0700

    add app; spotify, yelp, omdb

---
+++ liri.js
@@ -0,0 +1,86 @@
+
+require('dotenv').config();
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var axios = require('axios');
+var yelp = require('yelp-fusion');
+var spotify = new Spotify(keys.spotify);
+// console.log(keys.spotify)
+
+// argument
+var app = process.argv[2]
+var args = process.argv.slice(3);
+console.log(app, args)
+
+// [node] [liri] [api] [search]
+if (app == 'movie') {
+    // Then run a request with axios to the OMDB API with the movie specified
+    var queryUrl = 'http://www.omdbapi.com/?t=' + args + '&y=&plot=short&apikey=' + keys.OMDB_KEY;
+
+    // axios get, then, catch
+    axios.get(queryUrl)
+        .then(function (response) {
+            console.log(response.data);
+            console.log(`Year: ${response.data.Year}`);
+
+        }).catch(function (error) {
+            console.log(error);
+        });
+    console.log(keys.OMDB_KEY)
+} else if (app == 'spotify') {
+    spotify.search(
+        {
+            type: 'track',
+            query: args
+        },
+        function (err, data) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            var songs = data.tracks.items;
+            console.log(songs)
+        }
+    )
+} else if (app == 'yelp') {
+    var searchRequest = {
+        term: process.argv[3],
+        location: process.argv[4]
+    };
+
+    var apiKey = YELP_KEY;
+
+    var client = yelp.client(apiKey);
+
+    client.search(searchRequest)
+        .then(response => {
+            var firstResult = response.jsonBody.businesses[0];
+            var prettyJson = JSON.stringify(firstResult, null, 4);
+            console.log(prettyJson);
+        }).catch(err => {
+            console.log(err);
+        });
+}
+
+//FS READ WRITE
+// var fs = require('fs');
+
+// takes in name of file to write to
+// fs.writeFile('litfam.txt', 'fam its lit, star war, star trek', function (err) {
+//     if (err) return console.log(err);
+//     console.log('file write success');
+// });
+
+// 
+// fs.readFile('./best_things_ever.txt', 'utf8', function (err, data) {
+//     if (err) return console.log(err);
+//     // split string into array
+//     var dataSplit = data.split(',');
+//     var dataNew = dataSplit.map(function (el) {
+//         return el.trim();
+//     })
+
+//     console.log(dataNew);
+
+//     data.forEach(item)
+// })
