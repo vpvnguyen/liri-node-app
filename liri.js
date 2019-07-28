a7e4a0842185b6dbb0debf1afca64fa2076c4c2b
Vincent Nguyen <vpvnguyen@gmail.com>
2019-07-27 17:10:18 -0700

    fully functional version

--- liri.js
+++ liri.js
@@ -9,15 +9,24 @@ var fs = require('fs');
 var spotify = new Spotify(keys.spotify);
 // console.log(keys.spotify)
 
-// argument
+// arguments to be passed in
 var app = process.argv[2];
 var args = process.argv.slice(3);
-console.log(app, args);
 
+function userInput(app, args) {
+    console.log(`USER INPUT: ${app} | ${args}`)
+    // check if user input 
+    if (app === '') {
+        console.log('there are arguments')
+    } else {
+        console.log('no arguments')
+    }
+}
+userInput(app, args);
 // [node] [liri] [api] [search]
 if (app == 'movie') {
     // Then run a request with axios to the OMDB API with the movie specified
-    var queryUrl = 'http://www.omdbapi.com/?t=' + args + '&y=&plot=short&apikey=' + keys.OMDB_KEY;
+    var queryUrl = `http://www.omdbapi.com/?t=${args}&y=&plot=short&apikey=${keys.OMDB_KEY}`;
 
     // axios get, then, catch
     axios.get(queryUrl)
@@ -28,20 +37,38 @@ if (app == 'movie') {
         }).catch(function (error) {
             console.log(error);
         });
-    console.log(keys.OMDB_KEY);
 } else if (app == 'spotify') {
+    // If no song is provided then your program will default to "The Sign" by Ace of Base.
+    if (!args) {
+        console.log(args)
+        args = 'the sign';
+
+    }
     spotify.search(
         {
             type: 'track',
-            query: args
+            query: args,
+            limit: 1
         },
         function (err, data) {
             if (err) {
-                console.log(err);
+                // console.log(err);
                 return;
             }
             var songs = data.tracks.items;
-            console.log(songs);
+            // console.log(data);
+            // spotify
+            // Artist name
+            console.log(`Artist's name: ${JSON.stringify(songs[0].artists[0].name, null, 2)}`)
+            // The song's name name
+            console.log(`Song's name: ${JSON.stringify(songs[0].name, null, 2)}`)
+
+            // A preview link of the song from Spotify - preview_url
+            console.log(`Preview url: ${JSON.stringify(songs[0].preview_url, null, 2)}`)
+
+            // The album that the song is from - album.name
+            console.log(`Album: ${JSON.stringify(songs[0].album.name, null, 2)}`)
+
         }
     )
 } else if (app == 'yelp') {
@@ -63,6 +90,8 @@ if (app == 'movie') {
             console.log(err);
         });
 } else if (app == 'do-what-it-says') {
+    //FS READ from random.txt
+
     fs.readFile('./random.txt', 'utf8', function (err, data) {
         if (err) return console.log(err);
 
@@ -78,7 +107,6 @@ if (app == 'movie') {
     });
 };
 
-//FS READ WRITE
 
 // takes in name of file to write to
 // fs.writeFile('litfam.txt', 'fam its lit, star war, star trek', function (err) {
@@ -86,5 +114,17 @@ if (app == 'movie') {
 //     console.log('file write success');
 // });
 
-// 
+
+
+
+// * Title of the movie.
+// * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+//   if the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
 
