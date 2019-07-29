87de1eae41a649a10ec7e147a7d391fe67042885
Vincent Nguyen <vpvnguyen@gmail.com>
2019-07-28 20:37:02 -0700

    removed if else version; added functional version;

--- liri.js
+++ liri.js
@@ -7,83 +7,48 @@ var yelp = require('yelp-fusion');
 var fs = require('fs');
 
 var spotify = new Spotify(keys.spotify);
-// console.log(keys.spotify)
 
 // arguments to be passed in
 var app = process.argv[2];
 var args = process.argv.slice(3);
+var isSearchEmpty;
 
-function isAppEmpty(app) {
-    if (app === undefined) {
-        console.log('TRY: [node] [liri] [movie/spotify/yelp] [search]')
-    } else {
-        return app;
+// OMDB
+function runOMDB(args) {
+    if (isSearchEmpty === false) {
+        // if no arguments, set default
+        args = 'Mr. Nobody';
     }
-}
 
-function isArgsEmpty(args) {
-    if (args.length === 0) {
-        console.log('ARGS IS EMPTY')
-    } else {
-        return args;
-    }
-}
-isAppEmpty(app);
-isArgsEmpty(args);
-
-
-// function isEmpty(args) {
-//     for (var key in app, keys) {
-//         if (app.hasOwnProperty(key))
-//             return false;
-//     }
-//     return true;
-// }
-
-// function userInput(app, args) {
-//     console.log(`USER INPUT: ${app} | ${args}`)
-//     // check if user input 
-//     if (app === 'movie') {
-//         console.log(`APPLICATION: ${app}`)
-//         omdb();
-
-//     } else if (app === 'spotify') {
-//         console.log(`APPLICATION: ${app}`)
-//     } else if (app === 'yelp') {
-//         console.log(`APPLICATION: ${app}`)
-//     }
-// }
-// userInput(app, args);
-
-
-function omdb(app, args) {
-    console.log('OMDB()')
-    if (args === '') {
-        console.log('empty')
-    }
-}
-
-
-// [node] [liri] [api] [search]
-if (app == 'movie') {
     // Then run a request with axios to the OMDB API with the movie specified
     var queryUrl = `http://www.omdbapi.com/?t=${args}&y=&plot=short&apikey=${keys.OMDB_KEY}`;
 
     // axios get, then, catch
     axios.get(queryUrl)
         .then(function (response) {
-            console.log(response.data);
+
+            // log movie information
+            console.log(`Title: ${response.data.Title}`);
             console.log(`Year: ${response.data.Year}`);
+            console.log(`IMDB Rating: ${response.data.imdbRating}`);
+            console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+            console.log(`Country: ${response.data.Country}`);
+            console.log(`Language: ${response.data.Language}`);
+            console.log(`Plot: ${response.data.Plot}`);
+            console.log(`Actors: ${response.data.Actors}`);
+            console.log('\n');
 
         }).catch(function (error) {
             console.log(error);
+            console.log('Try: [node] [liri] [spotify] [song name]');
         });
-} else if (app == 'spotify') {
-    // If no song is provided then your program will default to "The Sign" by Ace of Base.
-    if (!args) {
-        console.log(args)
-        args = 'the sign';
+};
 
+// spotify
+function runSpotify(args) {
+    if (isSearchEmpty === false) {
+        // if search is empty, set default
+        args = 'i want it that way';
     }
     spotify.search(
         {
@@ -93,79 +58,91 @@ if (app == 'movie') {
         },
         function (err, data) {
             if (err) {
-                // console.log(err);
+                console.log('Try: [node] [liri] [spotify] [song name]');
                 return;
             }
             var songs = data.tracks.items;
-            // console.log(data);
-            // spotify
-            // Artist name
-            console.log(`Artist's name: ${JSON.stringify(songs[0].artists[0].name, null, 2)}`)
-            // The song's name name
-            console.log(`Song's name: ${JSON.stringify(songs[0].name, null, 2)}`)
 
-            // A preview link of the song from Spotify - preview_url
-            console.log(`Preview url: ${JSON.stringify(songs[0].preview_url, null, 2)}`)
-
-            // The album that the song is from - album.name
-            console.log(`Album: ${JSON.stringify(songs[0].album.name, null, 2)}`)
-
+            // log artist, song, preview link, album
+            console.log(`Artist's name: ${JSON.stringify(songs[0].artists[0].name, null, 2)}`);
+            console.log(`Song's name: ${JSON.stringify(songs[0].name, null, 2)}`);
+            console.log(`Preview url: ${JSON.stringify(songs[0].preview_url, null, 2)}`);
+            console.log(`Album: ${JSON.stringify(songs[0].album.name, null, 2)}`);
+            console.log('\n');
         }
     )
-} else if (app == 'yelp') {
+};
+
+// yelp-fusion
+function runYelp(args) {
+
+    // create header to search for business name and location
     var searchRequest = {
         term: process.argv[3],
         location: process.argv[4]
     };
 
-    var apiKey = keys.YELP_KEY;
+    var client = yelp.client(keys.YELP_KEY);
 
-    var client = yelp.client(apiKey);
-
+    // search for business information
     client.search(searchRequest)
         .then(response => {
             var firstResult = response.jsonBody.businesses[0];
-            var prettyJson = JSON.stringify(firstResult, null, 4);
-            console.log(prettyJson);
-        }).catch(err => {
-            console.log(err);
-        });
-} else if (app == 'do-what-it-says') {
-    //FS READ from random.txt
+            // var prettyJson = JSON.stringify(firstResult, null, 4);
+            // console.log(prettyJson);
+            console.log(`Name: ${firstResult.name}`);
+            console.log(`Is it closed?: ${firstResult.is_closed}`);
+            console.log(`Rating: ${firstResult.rating}`);
+            console.log(`Price: ${firstResult.price}`);
+            console.log(`Location: ${firstResult.location.address1}`);
+            console.log(`${firstResult.location.city}`);
+            console.log(`${firstResult.location.zip_code}`);
+            console.log(`${firstResult.location.state}`);
+            console.log('\n');
 
+        }).catch(err => {
+            console.log('Try: [node] [liri] [yelp] [name] [city]');
+        });
+};
+
+function doWhatItSays() {
     fs.readFile('./random.txt', 'utf8', function (err, data) {
         if (err) return console.log(err);
 
         var dataSplit = data.split(',');
-        var dataNew = dataSplit.map(function (el) {
-            return el.trim();
-        });
-
-        dataNew.forEach(function (item, index, arr) {
-            console.log(item);
-        });
-
+        runApplication(dataSplit[0], dataSplit[1]);
     });
 };
 
+// check if arguments are empty
+function isArgsEmpty(args) {
+    if (args.length === 0) {
+        isSearchEmpty = false;
+    } else {
+        isSearchEmpty = true;
+    }
+};
 
-// takes in name of file to write to
-// fs.writeFile('litfam.txt', 'fam its lit, star war, star trek', function (err) {
-//     if (err) return console.log(err);
-//     console.log('file write success');
-// });
+// check user input and runs appropriate app
+function runApplication(app, args) {
+    if (app === undefined) {
+        console.log('TRY: [node] [liri] [movie-this/spotify-this-song/yelp] [search]');
+    } else if (app === 'movie-this') {
+        runOMDB(args);
+    } else if (app === 'spotify-this-song') {
+        runSpotify(args);
+    } else if (app === 'yelp') {
+        runYelp(args);
+    } else if (app === 'do-what-it-says') {
+        doWhatItSays();
+    } else {
+        console.log('TRY: [node] [liri] [movie-this/spotify-this-song/yelp] [search]');
+    }
+};
 
+// run liri
+isArgsEmpty(args);
+runApplication(app, args);
 
-
-
-// * Title of the movie.
-// * Year the movie came out.
-//   * IMDB Rating of the movie.
-//   * Rotten Tomatoes Rating of the movie.
-//   * Country where the movie was produced.
-//   * Language of the movie.
-//   * Plot of the movie.
-//   * Actors in the movie.
-//   if the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
 
